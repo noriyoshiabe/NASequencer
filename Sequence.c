951ba0394a9a7f5464eed1751a4e11f07ac62ae5
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-01-21 22:41:12 +0900

    sequence data structure

--- Sequence.c
+++ Sequence.c
@@ -1,5 +1,6 @@
 #include "Sequence.h"
 #include <NACFHelper.h>
+#include <string.h>
 
 static void *__SequenceInit(void *_self, ...)
 {
@@ -130,4 +131,230 @@ NAClass TrackClass = {
 };
 
 
+static int __MidiEventCompare(const void *self, const void *to)
+{
+    return ((MidiEvent *)self)->tick - ((MidiEvent *)to)->tick;
+}
+
 char MidiEventID[] = "MidiEvent";
+
+
+static void *__TimeEventInit(void *_self, ...)
+{
+    TimeEvent *self = _self;
+    va_list ap;
+    va_start(ap, _self);
+    self->_.tick = va_arg(ap, uint32_t);
+    self->numerator = (uint8_t)va_arg(ap, uint32_t);
+    self->denominator = (uint8_t)va_arg(ap, uint32_t);
+    va_end(ap);
+
+    return self;
+}
+
+static void *__TimeEventDescription(const void *_self)
+{
+    const TimeEvent *self = _self;
+    return (void *)CFStringCreateWithFormat(NULL, NULL, CFSTR("<TimeEvent: tick=%d numerator=%d denominator=%d>"), self->_.tick, self->numerator, self->denominator);
+}
+
+static NATypeVtbl __TimeEvent__typeVtbl = {
+    __TimeEventInit,
+    NULL,
+    NULL,
+    NULL,
+    __MidiEventCompare,
+    NULL,
+    __TimeEventDescription
+};
+
+static NAVtblEntry __TimeEvent__vEntries[] = {
+    {NATypeID, &__TimeEvent__typeVtbl},
+    {NULL, NULL},
+};
+
+char TimeEventID[] = "TimeEvent";
+NAClass TimeEventClass = {
+    TimeEventID,
+    sizeof(TimeEvent),
+    __TimeEvent__vEntries,
+};
+
+
+static void *__TempoEventInit(void *_self, ...)
+{
+    TempoEvent *self = _self;
+    va_list ap;
+    va_start(ap, _self);
+    self->_.tick = va_arg(ap, uint32_t);
+    self->tempo = (float)va_arg(ap, double);
+    va_end(ap);
+
+    return self;
+}
+
+static void *__TempoEventDescription(const void *_self)
+{
+    const TempoEvent *self = _self;
+    return (void *)CFStringCreateWithFormat(NULL, NULL, CFSTR("<TempoEvent: tick=%d tempo=%f>"), self->_.tick, self->tempo);
+}
+
+static NATypeVtbl __TempoEvent__typeVtbl = {
+    __TempoEventInit,
+    NULL,
+    NULL,
+    NULL,
+    __MidiEventCompare,
+    NULL,
+    __TempoEventDescription
+};
+
+static NAVtblEntry __TempoEvent__vEntries[] = {
+    {NATypeID, &__TempoEvent__typeVtbl},
+    {NULL, NULL},
+};
+
+char TempoEventID[] = "TempoEvent";
+NAClass TempoEventClass = {
+    TempoEventID,
+    sizeof(TempoEvent),
+    __TempoEvent__vEntries,
+};
+
+
+static void *__MarkerEventInit(void *_self, ...)
+{
+    MarkerEvent *self = _self;
+    va_list ap;
+    va_start(ap, _self);
+    self->_.tick = va_arg(ap, uint32_t);
+    const char *text = va_arg(ap, const char *);
+    va_end(ap);
+
+    self->text = malloc(strlen(text) + 1);
+    strcpy(self->text, text);
+
+    return self;
+}
+
+static void __MarkerEventDestroy(void *_self)
+{
+    MarkerEvent *self = _self;
+    free(self->text);
+}
+
+static void *__MarkerEventDescription(const void *_self)
+{
+    const MarkerEvent *self = _self;
+    return (void *)CFStringCreateWithFormat(NULL, NULL, CFSTR("<MarkerEvent: tick=%d text=%s>"), self->_.tick, self->text);
+}
+
+static NATypeVtbl __MarkerEvent__typeVtbl = {
+    __MarkerEventInit,
+    __MarkerEventDestroy,
+    NULL,
+    NULL,
+    __MidiEventCompare,
+    NULL,
+    __MarkerEventDescription
+};
+
+static NAVtblEntry __MarkerEvent__vEntries[] = {
+    {NATypeID, &__MarkerEvent__typeVtbl},
+    {NULL, NULL},
+};
+
+char MarkerEventID[] = "MarkerEvent";
+NAClass MarkerEventClass = {
+    MarkerEventID,
+    sizeof(MarkerEvent),
+    __MarkerEvent__vEntries,
+};
+
+
+static void *__SoundSelectEventInit(void *_self, ...)
+{
+    SoundSelectEvent *self = _self;
+    va_list ap;
+    va_start(ap, _self);
+    self->_.tick = va_arg(ap, uint32_t);
+    self->channel = (uint8_t)va_arg(ap, uint32_t);
+    self->msb = (uint8_t)va_arg(ap, uint32_t);
+    self->lsb = (uint8_t)va_arg(ap, uint32_t);
+    self->programNo = (uint8_t)va_arg(ap, uint32_t);
+    va_end(ap);
+
+    return self;
+}
+
+static void *__SoundSelectEventDescription(const void *_self)
+{
+    const SoundSelectEvent *self = _self;
+    return (void *)CFStringCreateWithFormat(NULL, NULL, CFSTR("<SoundSelectEvent: tick=%d channel=%d msb=%d lsb=%d programNo=%d>"), self->_.tick, self->channel, self->msb, self->lsb, self->programNo);
+}
+
+static NATypeVtbl __SoundSelectEvent__typeVtbl = {
+    __SoundSelectEventInit,
+    NULL,
+    NULL,
+    NULL,
+    __MidiEventCompare,
+    NULL,
+    __SoundSelectEventDescription
+};
+
+static NAVtblEntry __SoundSelectEvent__vEntries[] = {
+    {NATypeID, &__SoundSelectEvent__typeVtbl},
+    {NULL, NULL},
+};
+
+char SoundSelectEventID[] = "SoundSelectEvent";
+NAClass SoundSelectEventClass = {
+    SoundSelectEventID,
+    sizeof(SoundSelectEvent),
+    __SoundSelectEvent__vEntries,
+};
+
+
+static void *__NoteEventInit(void *_self, ...)
+{
+    NoteEvent *self = _self;
+    va_list ap;
+    va_start(ap, _self);
+    self->_.tick = va_arg(ap, uint32_t);
+    self->channel = (uint8_t)va_arg(ap, uint32_t);
+    self->noteNo = (uint8_t)va_arg(ap, uint32_t);
+    self->velocity = (uint8_t)va_arg(ap, uint32_t);
+    self->gatetime = va_arg(ap, uint32_t);
+    va_end(ap);
+
+    return self;
+}
+
+static void *__NoteEventDescription(const void *_self)
+{
+    const NoteEvent *self = _self;
+    return (void *)CFStringCreateWithFormat(NULL, NULL, CFSTR("<NoteEvent: tick=%d channel=%d noteNo=%d velocity=%d gatetime=%d>"), self->_.tick, self->channel, self->noteNo, self->velocity, self->gatetime);
+}
+
+static NATypeVtbl __NoteEvent__typeVtbl = {
+    __NoteEventInit,
+    NULL,
+    NULL,
+    NULL,
+    __MidiEventCompare,
+    NULL,
+    __NoteEventDescription
+};
+
+static NAVtblEntry __NoteEvent__vEntries[] = {
+    {NATypeID, &__NoteEvent__typeVtbl},
+    {NULL, NULL},
+};
+
+char NoteEventID[] = "NoteEvent";
+NAClass NoteEventClass = {
+    NoteEventID,
+    sizeof(NoteEvent),
+    __NoteEvent__vEntries,
+};
--- Sequence.h
+++ Sequence.h
@@ -31,3 +31,45 @@ typedef struct _MidiEvent {
     uint32_t tick;
 } MidiEvent;
 extern char MidiEventID[];
+
+typedef struct _TimeEvent {
+    MidiEvent _;
+    uint8_t numerator;
+    uint8_t denominator;
+} TimeEvent;
+extern NAClass TimeEventClass;
+extern char TimeEventID[];
+
+typedef struct _TempoEvent {
+    MidiEvent _;
+    float tempo;
+} TempoEvent;
+extern char TempoEventID[];
+
+typedef struct _MarkerEvent {
+    MidiEvent _;
+    char *text;
+} MarkerEvent;
+extern NAClass MarkerEventClass;
+extern char MarkerEventID[];
+
+typedef struct _SoundSelectEvent {
+    MidiEvent _;
+    uint8_t channel;
+    uint8_t msb;
+    uint8_t lsb;
+    uint8_t programNo;
+} SoundSelectEvent;
+extern NAClass SoundSelectEventClass;
+extern char SoundSelectEventID[];
+
+typedef struct _NoteEvent {
+    MidiEvent _;
+    uint8_t channel;
+    uint8_t noteNo;
+    uint8_t velocity;
+    uint32_t gatetime;
+} NoteEvent;
+extern NAClass NoteEventClass;
+extern char NoteEventID[];
+
