6ec97a429eb5db07c026e557b1f5f340fe22cce9
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-01-21 21:34:38 +0900

    description methods

--- Sequence.c
+++ Sequence.c
@@ -17,6 +17,12 @@ static void __SequenceDestroy(void *_self)
     }
 }
 
+static void *__SequenceDescription(const void *_self)
+{
+    const Sequence *self = _self;
+    return (void *)CFStringCreateWithFormat(NULL, NULL, CFSTR("<Sequence: resolution=%d title=%s tracks=%@>"), self->resolution, self->title, self->tracks);
+}
+
 static NATypeVtbl __Sequence__typeVtbl = {
     __SequenceInit,
     __SequenceDestroy,
@@ -24,7 +30,7 @@ static NATypeVtbl __Sequence__typeVtbl = {
     NULL,
     NULL,
     NULL,
-    NULL, // TODO
+    __SequenceDescription,
 };
 
 static NAVtblEntry __Sequence__vEntries[] = {
@@ -37,6 +43,48 @@ NAClass SequenceClass = {
     SequenceID,
     sizeof(Sequence),
     __Sequence__vEntries,
+};
+
+
+static void *__PatternInit(void *_self, ...)
+{
+    Pattern *self = _self;
+    self->tracks = CFArrayCreateMutable(NULL, 0, NACFArrayCallBacks);
+    return self;
+}
+
+static void __PatternDestroy(void *_self)
+{
+    Pattern *self = _self;
+    CFRelease(self->tracks);
+}
+
+static void *__PatternDescription(const void *_self)
+{
+    const Pattern *self = _self;
+    return (void *)CFStringCreateWithFormat(NULL, NULL, CFSTR("<Pattern: tracks=%@>"), self->tracks);
+}
+
+static NATypeVtbl __Pattern__typeVtbl = {
+    __PatternInit,
+    __PatternDestroy,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    __PatternDescription,
+};
+
+static NAVtblEntry __Pattern__vEntries[] = {
+    {NATypeID, &__Pattern__typeVtbl},
+    {NULL, NULL},
+};
+
+char PatternID[] = "Pattern";
+NAClass PatternClass = {
+    PatternID,
+    sizeof(Pattern),
+    __Pattern__vEntries,
 };
 
 
@@ -53,6 +101,12 @@ static void __TrackDestroy(void *_self)
     CFRelease(self->events);
 }
 
+static void *__TrackDescription(const void *_self)
+{
+    const Track *self = _self;
+    return (void *)CFStringCreateWithFormat(NULL, NULL, CFSTR("<Track: events=%@>"), self->events);
+}
+
 static NATypeVtbl __Track__typeVtbl = {
     __TrackInit,
     __TrackDestroy,
@@ -60,7 +114,7 @@ static NATypeVtbl __Track__typeVtbl = {
     NULL,
     NULL,
     NULL,
-    NULL, // TODO
+    __TrackDescription
 };
 
 static NAVtblEntry __Track__vEntries[] = {
