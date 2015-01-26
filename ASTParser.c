47a14e3c4aae8d904760e4d283669990be227ab9
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-01-26 18:22:00 +0900

    revise context and recursive function

--- ASTParser.c
+++ ASTParser.c
@@ -1,51 +1,65 @@
 #include "ASTParser.h"
 #include "Parser.h"
+#include <NACFHelper.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-typedef struct _ParseContext {
+typedef struct _Context {
     Sequence *sequence;
-    int patternNest;
-} ParseContext;
+    uint32_t tick;
+    int32_t channel;
+    int32_t gatetime;
+    CFMutableArrayRef events;
+    //TimeTable *timeTable;
+} Context;
+
+static Context *createContext(Sequence *sequence)
+{
+    Context *context = calloc(1, sizeof(Context));
+    context->sequence = sequence;
+    context->events = CFArrayCreateMutable(NULL, 0, NACFArrayCallBacks);
+    // TODO TimeTable
+    return context;
+}
+
+static void destroyContext(Context *context)
+{
+    CFRelease(context->events);
+    // TODO TimeTable
+    free(context);
+}
 
 #define TABLE_SIZE (TOKEN_END - TOKEN_BEGIN)
 #define IDX(type) (type - TOKEN_BEGIN - 1)
 #define SET_ERROR(error, _kind, _expression, _message) (error->kind = _kind, error->expression = _expression, error->message = _message ? _message : "")
 
-static bool (*dispatchTable[TABLE_SIZE])(Expression *, ParseContext *, ASTParserError *) = {NULL};
+static bool (*dispatchTable[TABLE_SIZE])(Expression *, Context *, void *, ASTParserError *) = {NULL};
 
-static bool parseExpression(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool parseExpression(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
-    bool (*function)(Expression *, ParseContext *, ASTParserError *)= dispatchTable[IDX(expression->tokenType)];
+    bool (*function)(Expression *, Context *, void *, ASTParserError *)= dispatchTable[IDX(expression->tokenType)];
     if (!function) {
         fprintf(stderr, "Dispatch function is not found. tokenType=%s\n", tokenType2String(expression->tokenType));
         abort();
     }
-    return function(expression, context, error);
-    
-}
-
-static void destroyParseContext(ParseContext *context)
-{
-    free(context);
+    return function(expression, context, value, error);
 }
 
 Sequence *ASTParserParseExpression(Expression *expression, const char *filepath, ASTParserError *error)
 {
     Sequence *ret = NULL;
 
-    ParseContext *context = calloc(1, sizeof(ParseContext));
-    context->sequence = NATypeNew(Sequence);
+    Context *context = createContext(NATypeNew(Sequence));
 
     error->filepath = filepath;
 
     Expression *expr = expression;
 
     do {
-        if (!parseExpression(expr, context, error)) {
+        if (!parseExpression(expr, context, NULL, error)) {
             NARelease(context->sequence);
             goto ERROR;
         }
@@ -54,326 +68,324 @@ Sequence *ASTParserParseExpression(Expression *expression, const char *filepath,
     ret = context->sequence;
 
 ERROR:
-    destroyParseContext(context);
+    destroyContext(context);
 
     return ret;
 }
 
-static bool __dispatch__INTEGER(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__INTEGER(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
-    printf("called __dispatch__INTEGER()\n");
+    *((int *)value) = expression->v.i;
     return true;
 }
 
-static bool __dispatch__FLOAT(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__FLOAT(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__FLOAT()\n");
     return true;
 }
 
-static bool __dispatch__STRING(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__STRING(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
-    printf("called __dispatch__STRING()\n");
+    *((char **)value) = strdup(expression->v.s);
     return true;
 }
 
 
-static bool __dispatch__NOTE_NO(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__NOTE_NO(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__NOTE_NO()\n");
     return true;
 }
 
-static bool __dispatch__LOCATION(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__LOCATION(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__LOCATION()\n");
     return true;
 }
 
-static bool __dispatch__MB_LENGTH(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__MB_LENGTH(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__MB_LENGTH()\n");
     return true;
 }
 
 
-static bool __dispatch__RESOLUTION(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__RESOLUTION(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     if (0 != context->sequence->resolution) {
         SET_ERROR(error, ASTPARSER_RESOLUTION_REDEFINED, expression, "resolution cannot be defined twice.");
         return false;
     }
 
-    context->sequence->resolution = expression->left->v.i;
-    return true;
+    return parseExpression(expression->left, context, &context->sequence->resolution, error);
 }
 
-static bool __dispatch__TITLE(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__TITLE(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     if (0 != context->sequence->title) {
         SET_ERROR(error, ASTPARSER_TITLE_REDEFINED, expression, "title cannot be defined twice.");
         return false;
     }
 
-    context->sequence->title = strdup(expression->left->v.s);
-    return true;
+    return parseExpression(expression->left, context, &context->sequence->title, error);
 }
 
-static bool __dispatch__TIME(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__TIME(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__TIME()\n");
     return true;
 }
 
-static bool __dispatch__TEMPO(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__TEMPO(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__TEMPO()\n");
     return true;
 }
 
-static bool __dispatch__MARKER(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__MARKER(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__MARKER()\n");
     return true;
 }
 
-static bool __dispatch__SOUND(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__SOUND(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__SOUND()\n");
     return true;
 }
 
-static bool __dispatch__SELECT(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__SELECT(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__SELECT()\n");
     return true;
 }
 
-static bool __dispatch__CHANNEL(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__CHANNEL(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__CHANNEL()\n");
     return true;
 }
 
-static bool __dispatch__VELOCITY(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__VELOCITY(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__VELOCITY()\n");
     return true;
 }
 
-static bool __dispatch__GATETIME(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__GATETIME(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__GATETIME()\n");
     return true;
 }
 
-static bool __dispatch__CUTOFF(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__CUTOFF(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__CUTOFF()\n");
     return true;
 }
 
-static bool __dispatch__NOTE(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__NOTE(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__NOTE()\n");
     return true;
 }
 
 
-static bool __dispatch__STEP(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__STEP(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__STEP()\n");
     return true;
 }
 
-static bool __dispatch__FROM(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__FROM(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__FROM()\n");
     return true;
 }
 
-static bool __dispatch__TO(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__TO(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__TO()\n");
     return true;
 }
 
-static bool __dispatch__REPLACE(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__REPLACE(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__REPLACE()\n");
     return true;
 }
 
-static bool __dispatch__MIX(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__MIX(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__MIX()\n");
     return true;
 }
 
-static bool __dispatch__OFFSET(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__OFFSET(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__OFFSET()\n");
     return true;
 }
 
-static bool __dispatch__LENGTH(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__LENGTH(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__LENGTH()\n");
     return true;
 }
 
 
-static bool __dispatch__REST(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__REST(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__REST()\n");
     return true;
 }
 
-static bool __dispatch__TIE(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__TIE(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__TIE()\n");
     return true;
 }
 
 
-static bool __dispatch__PLUS(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__PLUS(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__PLUS()\n");
     return true;
 }
 
-static bool __dispatch__MINUS(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__MINUS(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__MINUS()\n");
     return true;
 }
 
-static bool __dispatch__DIVISION(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__DIVISION(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__DIVISION()\n");
     return true;
 }
 
-static bool __dispatch__MULTIPLY(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__MULTIPLY(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__MULTIPLY()\n");
     return true;
 }
 
-static bool __dispatch__ASSIGN(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__ASSIGN(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__ASSIGN()\n");
     return true;
 }
 
 
-static bool __dispatch__SEMICOLON(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__SEMICOLON(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__SEMICOLON()\n");
     return true;
 }
 
-static bool __dispatch__COMMA(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__COMMA(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__COMMA()\n");
     return true;
 }
 
 
-static bool __dispatch__LPAREN(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__LPAREN(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__LPAREN()\n");
     return true;
 }
 
-static bool __dispatch__RPAREN(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__RPAREN(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__RPAREN()\n");
     return true;
 }
 
-static bool __dispatch__LCURLY(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__LCURLY(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__LCURLY()\n");
     return true;
 }
 
-static bool __dispatch__RCURLY(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__RCURLY(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__RCURLY()\n");
     return true;
 }
 
 
-static bool __dispatch__IDENTIFIER(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__IDENTIFIER(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__IDENTIFIER()\n");
     return true;
 }
 
 
-static bool __dispatch__EOL(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__EOL(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__EOL()\n");
     return true;
 }
 
 
-static bool __dispatch__TIME_SIGN(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__TIME_SIGN(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__TIME_SIGN()\n");
     return true;
 }
 
-static bool __dispatch__SOUND_SELECT(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__SOUND_SELECT(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__SOUND_SELECT()\n");
     return true;
 }
 
-static bool __dispatch__INTEGER_LIST(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__INTEGER_LIST(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__INTEGER_LIST()\n");
     return true;
 }
 
-static bool __dispatch__GATETIME_CUTOFF(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__GATETIME_CUTOFF(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__GATETIME_CUTOFF()\n");
     return true;
 }
 
-static bool __dispatch__NOTE_BLOCK(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__NOTE_BLOCK(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__NOTE_BLOCK()\n");
     return true;
 }
 
-static bool __dispatch__NOTE_NO_LIST(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__NOTE_NO_LIST(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__NOTE_NO_LIST()\n");
     return true;
 }
 
-static bool __dispatch__PATTERN_DEFINE(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__PATTERN_DEFINE(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__PATTERN_DEFINE()\n");
     return true;
 }
 
-static bool __dispatch__PATTERN_BLOCK(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__PATTERN_BLOCK(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__PATTERN_BLOCK()\n");
     return true;
 }
 
-static bool __dispatch__PATTERN_EXPAND(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__PATTERN_EXPAND(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__PATTERN_EXPAND()\n");
     return true;
 }
 
-static bool __dispatch__PATTERN_EXTEND_BLOCK(Expression *expression, ParseContext *context, ASTParserError *error)
+static bool __dispatch__PATTERN_EXTEND_BLOCK(Expression *expression, Context *context, void *value, ASTParserError *error)
 {
     printf("called __dispatch__PATTERN_EXTEND_BLOCK()\n");
     return true;
