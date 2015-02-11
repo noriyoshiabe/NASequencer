39d9b8bb34b7bd9742fc3ea1a81dcaba4746e3a7
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-02-12 00:40:46 +0900

    implement message queue

---
+++ MessageQueue.c
@@ -0,0 +1,81 @@
+#include <MessageQueue.h>
+
+#include <stdlib.h>
+#include <pthread.h>
+
+#define MSGQ_SIZE 8
+
+struct _MessageQueue {
+    Message msgQ[MSGQ_SIZE];
+    int count;
+    int readIdx;
+    int writeIdx;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+};
+
+MessageQueue *MessageQueueCreate()
+{
+    MessageQueue *self = calloc(1, sizeof(MessageQueue));
+    pthread_mutex_init(&self->mutex, NULL);
+    pthread_cond_init(&self->cond, NULL);
+    return self;
+}
+
+void MessageQueueDestroy(MessageQueue *self)
+{
+    pthread_mutex_destroy(&self->mutex);
+    pthread_cond_destroy(&self->cond);
+    free(self);
+}
+
+bool MessageQueuePost(MessageQueue *self, const Message *message)
+{
+    pthread_mutex_lock(&self->mutex);
+
+    if (MSGQ_SIZE <= self->count) {
+        pthread_mutex_unlock(&self->mutex);
+        return false;
+    }
+
+    self->msgQ[self->writeIdx++] = *message;
+    ++self->count;
+    pthread_cond_signal(&self->cond);
+
+    pthread_mutex_unlock(&self->mutex);
+
+    return true;
+}
+
+bool MessageQueueWait(MessageQueue *self, Message *message)
+{
+    pthread_mutex_lock(&self->mutex);
+
+    if (0 == self->count) {
+        pthread_cond_wait(&self->cond, &self->mutex);
+    }
+
+    *message = self->msgQ[self->readIdx++];
+    --self->count;
+
+    pthread_mutex_unlock(&self->mutex);
+
+    return true;
+}
+
+bool MessageQueuePeek(MessageQueue *self, Message *message)
+{
+    pthread_mutex_lock(&self->mutex);
+
+    if (0 == self->count) {
+        pthread_mutex_unlock(&self->mutex);
+        return false;
+    }
+
+    *message = self->msgQ[self->readIdx++];
+    --self->count;
+
+    pthread_mutex_unlock(&self->mutex);
+
+    return true;
+}
