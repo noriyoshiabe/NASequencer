115491851d1fc8d6d841710eb756e5caede51fd2
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-02-14 23:49:39 +0900

    hook open menu

---
+++ experiment/namidi/namidi/Classes/Controllers/FileDialogController.swift
@@ -0,0 +1,25 @@
+//
+//  FileDialogController.swift
+//  namidi
+//
+//  Created by abechan on 2015/02/14.
+//
+//
+
+import Foundation
+import AppKit
+
+class FileDialogController : NSObject {
+    
+    @IBAction func onSelectOpen(sender: AnyObject) {
+        var openPanel = NSOpenPanel()
+        openPanel.allowedFileTypes = ["namidi"]
+        
+        switch (openPanel.runModal()) {
+        case NSOKButton:
+            println(NSString(format: "### I wanna open %@", openPanel.URLs))
+        default:
+            break
+        }
+    }
+}
