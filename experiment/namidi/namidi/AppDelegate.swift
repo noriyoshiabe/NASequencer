479f70f0c5c1d060c632c7aaf76dc1fa04750f68
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-02-14 22:02:00 +0900

    experiment project

---
+++ experiment/namidi/namidi/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  namidi
+//
+//  Created by abechan on 2015/02/14.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
