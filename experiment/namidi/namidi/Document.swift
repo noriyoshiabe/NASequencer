c97f94df171e5045b04a43ebff22f8aad49fa1dd
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-02-15 17:35:37 +0900

    disable autosavesInPlace

--- experiment/namidi/namidi/Document.swift
+++ experiment/namidi/namidi/Document.swift
@@ -20,10 +20,6 @@ class Document: NSDocument {
         // Add any code here that needs to be executed once the windowController has loaded the document's window.
     }
 
-    override class func autosavesInPlace() -> Bool {
-        return true
-    }
-
     override var windowNibName: String? {
         // Returns the nib file name of the document
         // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
