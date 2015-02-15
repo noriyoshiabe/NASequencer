a8e682c64b73d94d9c900c200af4ddc7215166f0
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-02-15 15:04:49 +0900

    create document based application

---
+++ experiment/namidi/namidi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  namidi
+//
+//  Created by abechan on 2015/02/15.
+//  Copyright (c) 2015年 abechan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
