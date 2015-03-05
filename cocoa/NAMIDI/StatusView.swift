068c46c1863cbb0f440807b1dfb00103cb806042
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-03-06 01:07:28 +0900

    optimize status view update

--- cocoa/NAMIDI/StatusView.swift
+++ cocoa/NAMIDI/StatusView.swift
@@ -17,53 +17,43 @@ class StatusView : NSView, NAMidiProxyDelegate {
 
     @IBOutlet var playing: NSTextField?
     
-    var parseContext: ParseContextAdapter?
-    var error: ParseError?
-    var context: PlayerContext?
+    var lastUsec: Int64 = 0
     
     func onParseFinished(namidi: NAMidiProxy, context: UnsafeMutablePointer<ParseContext>) {
-        self.parseContext = ParseContextAdapter(contextRef: context)
-        if self.parseContext!.hasError {
-            error = self.parseContext!.error
-        }
-        else {
-            error = nil
-        }
-        
-        setNeedsDisplayInRect(self.errorInfomation!.bounds)
-    }
-    
-    func onPlayerContextChanged(namidi: NAMidiProxy, context: UnsafeMutablePointer<PlayerContext>) {
-        self.context = context.memory
-        setNeedsDisplayInRect(self.playing!.bounds)
-    }
-    
-    override func drawRect(dirtyRect: NSRect) {
-        if nil != error {
-            errorKind!.stringValue = error!.message.takeUnretainedValue()
-            filename!.stringValue = (error!.filepath.takeUnretainedValue() as NSString).lastPathComponent
-            location!.stringValue = String(format: "Line: %d   Column: %d", error!.location.firstLine, error!.location.firstColumn)
+        let parseContext = ParseContextAdapter(contextRef: context)
+        if parseContext.hasError {
+            let error = parseContext.error
+            errorKind!.stringValue = error.message.takeUnretainedValue()
+            filename!.stringValue = (error.filepath.takeUnretainedValue() as NSString).lastPathComponent
+            location!.stringValue = String(format: "Line: %d   Column: %d", error.location.firstLine, error.location.firstColumn)
             errorInfomation!.hidden = false
         }
         else {
             errorInfomation!.hidden = true
         }
         
-        if nil == context {
+    }
+    
+    func onPlayerContextChanged(namidi: NAMidiProxy, context: UnsafeMutablePointer<PlayerContext>) {
+        let context = context.memory
+        
+        if 10000 > abs(context.usec - lastUsec) {
             return
         }
         
-        let min:Int = Int(context!.usec / (1000 * 1000 * 60))
-        let sec:Int = Int(context!.usec / (1000 * 1000))
-        let msec:Int = Int((context!.usec / 1000) % 1000)
+        let min:Int = Int(context.usec / (1000 * 1000 * 60))
+        let sec:Int = Int(context.usec / (1000 * 1000))
+        let msec:Int = Int((context.usec / 1000) % 1000)
         
         playing!.stringValue = String(format: "%02d:%02d:%03d   %03d:%02d:%03d",
             min,
             sec,
             msec,
-            context!.location.m,
-            context!.location.b,
-            context!.location.t
+            context.location.m,
+            context.location.b,
+            context.location.t
         )
+        
+        lastUsec = context.usec
     }
 }
