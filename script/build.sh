b505435f7042bf7a1cce3f83262fdd19bf3867b7
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-03-10 20:38:59 +0900

    build shell script

---
+++ script/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ROOT=$(cd $(dirname $0) && pwd)/..
+xcodebuild -project $ROOT/cocoa/NAMIDI.xcodeproj -configuration Release clean build SYMROOT=$ROOT/bin/Build
+mkdir $ROOT/release 2> /dev/null
+cp -r $ROOT/bin/Build/Release/NAMIDI.app $ROOT/release/
