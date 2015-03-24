cc84a4c6728382d677fead1787e85eed0d1f82f1
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-03-24 21:29:41 +0900

    organize files related help

--- help/build.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-
-require 'yaml'
-require 'erb'
-
-root = File.expand_path(File.dirname(__FILE__))
-directives = YAML.load_file("#{root}/directive.yml")
-syntax = ERB.new(File.read("#{root}/syntax.erb"), nil, '%')
-File.write("#{root}/syntax.html", syntax.result(binding))
