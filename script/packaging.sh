1162bbb8b7c14ccbee523c9a6da2917f668511e6
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-03-25 00:42:18 +0900

    fix application folder alias in dmg

--- script/packaging.sh
+++ script/packaging.sh
@@ -4,9 +4,9 @@ ROOT=$(cd $(dirname $0) && pwd)/..
 mkdir -p $ROOT/release/alpha 2> /dev/null
 cp $ROOT/sample/example.namidi $ROOT/release/alpha/
 cp $ROOT/sample/midi.trak $ROOT/release/alpha/
-cp $ROOT/release/Applications $ROOT/release/alpha/
 cp $ROOT/release/README.txt.alpha $ROOT/release/alpha/README.txt
 cp -r $ROOT/release/NAMIDI.app $ROOT/release/alpha/
+ln -s /Applications $ROOT/release/alpha/Applications
 
 rm $ROOT/release/"NAMIDI alpha.dmg" 2> /dev/null
 hdiutil create $ROOT/release/"NAMIDI alpha.dmg" -volname "NAMIDI Alpha" -fs HFS+ -srcfolder $ROOT/release/alpha
