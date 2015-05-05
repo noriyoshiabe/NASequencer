8b577354ca8bf4e99641413e6530df72b3a63a4a
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-05-05 13:41:23 +0900

    callback to application model from parser

--- scratch/NAMidi.m
+++ scratch/NAMidi.m
@@ -9,44 +9,50 @@
 
 @implementation NAMidi
 
-static void onParseNote(void *receiver, uint32_t tick, uint8_t channel, uint8_t noteNo, uint8_t velocity, uint32_t gatetime)
+static void _NAMidiParserOnParseNote(void *receiver, uint32_t tick, uint8_t channel, uint8_t noteNo, uint8_t velocity, uint32_t gatetime)
 {
-    printf("[Note] tick=%d channel=%d noteNo=%d velocity=%d gatetime=%d\n", tick, channel, noteNo, velocity, gatetime);
+    NAMidi *self = (__bridge NAMidi *)receiver;
+    [self parser:self->parser onParseNote:tick channel:channel noteNo:noteNo velocity:velocity gatetime:gatetime];
 }
 
-static void onParseTime(void *receiver, uint32_t tick, uint8_t numerator, uint8_t denominator)
+static void _NAMidiParserOnParseTime(void *receiver, uint32_t tick, uint8_t numerator, uint8_t denominator)
 {
-    printf("[Time] tick=%d numerator=%d denominator=%d\n", tick, numerator, denominator);
+    NAMidi *self = (__bridge NAMidi *)receiver;
+    [self parser:self->parser onParseTime:tick numerator:numerator denominator:denominator];
 }
 
-static void onParseTempo(void *receiver, uint32_t tick, float tempo)
+static void _NAMidiParserOnParseTempo(void *receiver, uint32_t tick, float tempo)
 {
-    printf("[Tempo] tick=%d tempo=%.2f\n", tick, tempo);
+    NAMidi *self = (__bridge NAMidi *)receiver;
+    [self parser:self->parser onParseTempo:tick tempo:tempo];
 }
 
-static void onParseMarker(void *receiver, uint32_t tick, const char *text)
+static void _NAMidiParserOnParseMarker(void *receiver, uint32_t tick, const char *text)
 {
-    printf("[Marker] tick=%d text=%s\n", tick, text);
+    NAMidi *self = (__bridge NAMidi *)receiver;
+    [self parser:self->parser onParseMarker:tick text:text];
 }
 
-static void onFinish(void *receiver, uint32_t length)
+static void _NAMidiParserOnFinish(void *receiver, uint32_t length)
 {
-    printf("onFinish() length=%d\n", length);
+    NAMidi *self = (__bridge NAMidi *)receiver;
+    [self parser:self->parser onFinish:length];
 }
 
-static void onError(void *receiver, const char *filepath, int line, int column, ParseError error, ...)
+static void _NAMidiParserOnError(void *receiver, const char *filepath, int line, int column, ParseError error, ...)
 {
-    printf("onError() filepath=%s line=%d column=%d error=%s\n", filepath, line, column, ParseError2String(error));
+    NAMidi *self = (__bridge NAMidi *)receiver;
+    [self parser:self->parser onError:filepath line:line column:column error:error];
 }
 
 static NAMidiParserCallbacks callbacks = {
-    onParseNote,
-    onParseTime,
-    onParseTempo,
-    onParseMarker,
+    _NAMidiParserOnParseNote,
+    _NAMidiParserOnParseTime,
+    _NAMidiParserOnParseTempo,
+    _NAMidiParserOnParseMarker,
 
-    onFinish,
-    onError,
+    _NAMidiParserOnFinish,
+    _NAMidiParserOnError,
 };
 
 - (id)init
@@ -65,6 +71,36 @@ static NAMidiParserCallbacks callbacks = {
 - (void)execute:(const char *)filepath
 {
     NAMidiParserExecuteParse(parser, filepath);
+}
+
+- (void)parser:(NAMidiParser *)parser onParseNote:(uint32_t)tick channel:(uint8_t)channel noteNo:(uint8_t)noteNo velocity:(uint8_t)velocity gatetime:(uint32_t)gatetime
+{
+    printf("[Note] tick=%d channel=%d noteNo=%d velocity=%d gatetime=%d\n", tick, channel, noteNo, velocity, gatetime);
+}
+
+- (void)parser:(NAMidiParser *)parser onParseTime:(uint32_t)tick numerator:(uint8_t)numerator denominator:(uint8_t)denominator
+{
+    printf("[Time] tick=%d numerator=%d denominator=%d\n", tick, numerator, denominator);
+}
+
+- (void)parser:(NAMidiParser *)parser onParseTempo:(uint32_t)tick tempo:(float)tempo
+{
+    printf("[Tempo] tick=%d tempo=%.2f\n", tick, tempo);
+}
+
+- (void)parser:(NAMidiParser *)parser onParseMarker:(uint32_t)tick text:(const char *)text
+{
+    printf("[Marker] tick=%d text=%s\n", tick, text);
+}
+
+- (void)parser:(NAMidiParser *)parser onFinish:(uint32_t)length
+{
+    printf("onFinish() length=%d\n", length);
+}
+
+- (void)parser:(NAMidiParser *)parser onError:(const char *)filepath line:(int)line column:(int)column error:(ParseError)error
+{
+    printf("onError() filepath=%s line=%d column=%d error=%s\n", filepath, line, column, ParseError2String(error));
 }
 
 @end
