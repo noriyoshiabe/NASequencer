49fd57fae42fe25f6f9ddcc3c28e399ebe39625e
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-06-20 23:33:04 +0900

    testing ui

--- research/main.c
+++ research/main.c
@@ -1,37 +1,51 @@
 #include "AudioOut.h"
 #include "Synthesizer.h"
 #include "MidiSource.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <math.h>
+#include <sys/select.h>
+#include <termios.h>
 
-typedef struct _SineWave {
-    float frequency;
-    float phase;
-} SineWave;
+struct termios orig_termios;
 
-#if 0
-static void _AudioCallback(void *receiver, AudioSample *buffer, uint32_t length)
+void reset_terminal_mode()
 {
-    SineWave *signWave = receiver;
-    float freq = signWave->frequency * 2 * M_PI / 44100.0f;
-
-    for (int i = 0; i < length; ++i) {
-        float wave = sin(signWave->phase) / 4;
-        buffer[i].L = buffer[i].R = wave;
-        signWave->phase += freq;
-    }
+    tcsetattr(0, TCSANOW, &orig_termios);
 }
 
-#else
+void set_conio_terminal_mode()
+{
+    struct termios new_termios;
+
+    /* take two copies - one for now, one for later */
+    tcgetattr(0, &orig_termios);
+    memcpy(&new_termios, &orig_termios, sizeof(new_termios));
+
+    /* register cleanup handler, and set the new terminal mode */
+    atexit(reset_terminal_mode);
+    cfmakeraw(&new_termios);
+    tcsetattr(0, TCSANOW, &new_termios);
+}
+
+int getch()
+{
+    int r;
+    unsigned char c;
+    if ((r = read(0, &c, sizeof(c))) < 0) {
+        return r;
+    } else {
+        return c;
+    }
+}
 
 static void _AudioCallback(void *receiver, AudioSample *buffer, uint32_t count)
 {
     Synthesizer *synth = receiver;
     SynthesizerComputeAudioSample(synth, buffer, count);
 }
-#endif
 
-#if 1
 int main(int argc, char **argv)
 {
     AudioOut *audioOut = AudioOutSharedInstance();
@@ -40,44 +54,161 @@ int main(int argc, char **argv)
     AudioOutRegisterCallback(audioOut, _AudioCallback, synth);
 
     MidiSource *midiSrc = (MidiSource *)synth;
-    midiSrc->send(midiSrc, NULL, 0);
 
-    sleep(1);
+    int presetCount;
+    midiSrc->getProperty(midiSrc, MidiSourcePropertyPresetCount, &presetCount);
 
-    midiSrc->send(midiSrc, NULL, 1);
+    PresetMap *presetMaps = calloc(presetCount, sizeof(PresetMap));
+    midiSrc->getProperty(midiSrc, MidiSourcePropertyPresetMap, presetMaps);
 
-    while (SynthesizerVoicingCount(synth)) {
-        usleep(100);
+    for (int i = 0; i < presetCount; ++i) {
+        PresetMap *presetMap = &presetMaps[i];
+        printf("preset: name=%s bankNo=%d, programNo=%d\n", presetMap->name, presetMap->bankNo, presetMap->programNo);
     }
+
+    set_conio_terminal_mode();
+
+    int octave = 4;
+    int presetIndex = 0;
+
+    uint8_t bytes[3];
+
+    bool noteon[128] = {0};
+
+    char c;
+    while (3 != (c = getch())) {
+        switch (c) {
+        case 27:
+            if (91 == getch()) {
+                c = getch();
+                switch (c) {
+                case 'A': // up
+                    {
+                        ++presetIndex;
+                        if (presetCount <= presetIndex) {
+                            presetIndex = 0;
+                        }
+
+                        PresetMap *presetMap = &presetMaps[presetIndex];
+
+                        bytes[0] = 0xB0;
+                        bytes[1] = 0x00;
+                        bytes[2] = presetMap->bankNo >> 8 & 0x00FF;
+                        midiSrc->send(midiSrc, bytes, 3);
+
+                        bytes[1] = 0x20;
+                        bytes[2] = presetMap->bankNo & 0x00FF;
+                        midiSrc->send(midiSrc, bytes, 3);
+
+                        bytes[0] = 0xC0;
+                        bytes[1] = presetMap->programNo;
+                        midiSrc->send(midiSrc, bytes, 2);
+
+                        printf("preset change: index=%d name=%s bank:%d ptrogram:%d\r\n", presetIndex,
+                                presetMap->name, presetMap->bankNo, presetMap->programNo);
+                    }
+                    break;
+                case 'B': // down
+                    {
+                        --presetIndex;
+                        if (0 > presetIndex) {
+                            presetIndex = presetCount - 1;
+                        }
+
+                        PresetMap *presetMap = &presetMaps[presetIndex];
+
+                        bytes[0] = 0xB0;
+                        bytes[1] = 0x00;
+                        bytes[2] = presetMap->bankNo >> 8 & 0x00FF;
+                        midiSrc->send(midiSrc, bytes, 3);
+
+                        bytes[1] = 0x20;
+                        bytes[2] = presetMap->bankNo & 0x00FF;
+                        midiSrc->send(midiSrc, bytes, 3);
+
+                        bytes[0] = 0xC0;
+                        bytes[1] = presetMap->programNo;
+                        midiSrc->send(midiSrc, bytes, 2);
+
+                        printf("preset change: index=%d name=%s bank:%d ptrogram:%d\r\n", presetIndex,
+                                presetMap->name, presetMap->bankNo, presetMap->programNo);
+                    }
+                    break;
+                case 'C': // right
+                    ++octave;
+                    if (8 < octave) {
+                        octave = 8;
+                    }
+                    else {
+                        printf("octave=%d\r\n", octave);
+                    }
+                    break;
+                case 'D': // left
+                    --octave;
+                    if (-2 > octave) {
+                        octave = -2;
+                    }
+                    else {
+                        printf("octave=%d\r\n", octave);
+                    }
+                    break;
+                }
+            }
+            break;
+        default:
+            {
+                int n = -99;
+
+                switch (c) {
+                case 'z': n = -3; break;
+                case 's': n = -2; break;
+                case 'x': n = -1; break;
+                case 'c': n = 0; break;
+                case 'f': n = 1; break;
+                case 'v': n = 2; break;
+                case 'g': n = 3; break;
+                case 'b': n = 4; break;
+                case 'n': n = 5; break;
+                case 'j': n = 6; break;
+                case 'm': n = 7; break;
+                case 'k': n = 8; break;
+                case ',': n = 9; break;
+                case 'l': n = 10; break;
+                case '.': n = 11; break;
+                case '/': n = 12; break;
+                }
+
+                if (n != -99) {
+                    int note = (octave + 2) * 12 + n;
+                    if (0 < note && note < 128) {
+                        if (noteon[note]) {
+                            bytes[0] = 0x80;
+                            bytes[1] = note;
+                            bytes[2] = 0;
+                            midiSrc->send(midiSrc, bytes, 3);
+
+                            printf("note off: channel=0 noteNo=%d velocity=0\r\n", note);
+                        }
+                        else {
+                            bytes[0] = 0x90;
+                            bytes[1] = note;
+                            bytes[2] = 127;
+                            midiSrc->send(midiSrc, bytes, 3);
+
+                            printf("note on: channel=0 noteNo=%d velocity=127\r\n", note);
+                        }
+
+                        noteon[note] = !noteon[note];
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    getch(); /* consume the character */
 
     AudioOutUnregisterCallback(audioOut, _AudioCallback, synth);
     SynthesizerDestroy(synth);
     return 0;
 }
-
-#else
-
-int main(int argc, char **argv)
-{
-    AudioOut *audioOut = AudioOutSharedInstance();
-
-    SineWave sw1 = {440, 0};
-    AudioOutRegisterCallback(audioOut, _AudioCallback, &sw1);
-    sleep(1);
-
-    SineWave sw2 = {880, 0};
-    AudioOutRegisterCallback(audioOut, _AudioCallback, &sw2);
-    sleep(1);
-
-    AudioOutUnregisterCallback(audioOut, _AudioCallback, &sw2);
-    sleep(1);
-
-    AudioOutUnregisterCallback(audioOut, _AudioCallback, &sw1);
-    sleep(1);
-
-    AudioOutRegisterCallback(audioOut, _AudioCallback, &sw1);
-    sleep(1);
-
-    return 0;
-}
-#endif
