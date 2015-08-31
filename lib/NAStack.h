7c401d2e45a9cfb054e2ae4851d81463205d8f95
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-08-31 23:20:14 +0900

    stack data type

---
+++ lib/NAStack.h
@@ -0,0 +1,8 @@
+#pragma once
+
+typedef struct _NAStack NAStack;
+
+extern NAStack *NAStackCreate(int initialSize);
+extern void NAStackDestroy(NAStack *self);
+extern void NAStackPush(NAStack *self, void *value);
+extern void *NAStackPop(NAStack *self);
