32c255f7356e10bdb0200a0203956cdbb9ac7878
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2015-12-30 01:04:14 +0900

    abc semantics data structure

---
+++ parser/abc/ABCSEM.c
@@ -0,0 +1,275 @@
+#include "ABCSEM.h"
+
+#include <stdlib.h>
+
+#define IF(p, destroy) do { if (p) { destroy(p); } } while (0)
+
+static void SEMFileAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitFile(visitor, self);
+}
+
+static void SEMFileDestroy(void *self)
+{
+}
+
+SEMFile *ABCSEMFileCreate(FileLocation *location)
+{
+    return NodeCreate(SEMFile, location);
+}
+static void SEMTuneAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTune(visitor, self);
+}
+
+static void SEMTuneDestroy(void *_self)
+{
+    SEMTune *self = _self;
+    IF(self->partSequence, free);
+    IF(self->title, free);
+}
+
+SEMTune *ABCSEMTuneCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTune, location);
+}
+static void SEMTempoAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTempo(visitor, self);
+}
+
+static void SEMTempoDestroy(void *self)
+{
+}
+
+SEMTempo *ABCSEMTempoCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTempo, location);
+}
+static void SEMTimeAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTime(visitor, self);
+}
+
+static void SEMTimeDestroy(void *self)
+{
+}
+
+SEMTime *ABCSEMTimeCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTime, location);
+}
+static void SEMKeyAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitKey(visitor, self);
+}
+
+static void SEMKeyDestroy(void *_self)
+{
+    SEMKey *self = _self;
+    IF(self->noteTable, NoteTableRelease);
+}
+
+SEMKey *ABCSEMKeyCreate(FileLocation *location)
+{
+    return NodeCreate(SEMKey, location);
+}
+static void SEMUnitNoteLengthAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitUnitNoteLength(visitor, self);
+}
+
+static void SEMUnitNoteLengthDestroy(void *self)
+{
+}
+
+SEMUnitNoteLength *ABCSEMUnitNoteLengthCreate(FileLocation *location)
+{
+    return NodeCreate(SEMUnitNoteLength, location);
+}
+static void SEMPartAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitPart(visitor, self);
+}
+
+static void SEMPartDestroy(void *_self)
+{
+    SEMPart *self = _self;
+    IF(self->identifier, free);
+}
+
+SEMPart *ABCSEMPartCreate(FileLocation *location)
+{
+    return NodeCreate(SEMPart, location);
+}
+static void SEMVoiceAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitVoice(visitor, self);
+}
+
+static void SEMVoiceDestroy(void *_self)
+{
+    SEMVoice *self = _self;
+    IF(self->identifier, free);
+}
+
+SEMVoice *ABCSEMVoiceCreate(FileLocation *location)
+{
+    return NodeCreate(SEMVoice, location);
+}
+static void SEMNoteAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitNote(visitor, self);
+}
+
+static void SEMNoteDestroy(void *self)
+{
+}
+
+SEMNote *ABCSEMNoteCreate(FileLocation *location)
+{
+    return NodeCreate(SEMNote, location);
+}
+static void SEMBrokenRhythmAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitBrokenRhythm(visitor, self);
+}
+
+static void SEMBrokenRhythmDestroy(void *self)
+{
+}
+
+SEMBrokenRhythm *ABCSEMBrokenRhythmCreate(FileLocation *location)
+{
+    return NodeCreate(SEMBrokenRhythm, location);
+}
+static void SEMRestAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitRest(visitor, self);
+}
+
+static void SEMRestDestroy(void *self)
+{
+}
+
+SEMRest *ABCSEMRestCreate(FileLocation *location)
+{
+    return NodeCreate(SEMRest, location);
+}
+static void SEMRepeatAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitRepeat(visitor, self);
+}
+
+static void SEMRepeatDestroy(void *self)
+{
+}
+
+SEMRepeat *ABCSEMRepeatCreate(FileLocation *location)
+{
+    return NodeCreate(SEMRepeat, location);
+}
+static void SEMBarLineAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitBarLine(visitor, self);
+}
+
+static void SEMBarLineDestroy(void *self)
+{
+}
+
+SEMBarLine *ABCSEMBarLineCreate(FileLocation *location)
+{
+    return NodeCreate(SEMBarLine, location);
+}
+static void SEMTieAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTie(visitor, self);
+}
+
+static void SEMTieDestroy(void *self)
+{
+}
+
+SEMTie *ABCSEMTieCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTie, location);
+}
+static void SEMGraceNoteAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitGraceNote(visitor, self);
+}
+
+static void SEMGraceNoteDestroy(void *self)
+{
+}
+
+SEMGraceNote *ABCSEMGraceNoteCreate(FileLocation *location)
+{
+    return NodeCreate(SEMGraceNote, location);
+}
+static void SEMTupletAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTuplet(visitor, self);
+}
+
+static void SEMTupletDestroy(void *self)
+{
+}
+
+SEMTuplet *ABCSEMTupletCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTuplet, location);
+}
+static void SEMChordAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitChord(visitor, self);
+}
+
+static void SEMChordDestroy(void *self)
+{
+}
+
+SEMChord *ABCSEMChordCreate(FileLocation *location)
+{
+    return NodeCreate(SEMChord, location);
+}
+static void SEMOverlayAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitOverlay(visitor, self);
+}
+
+static void SEMOverlayDestroy(void *self)
+{
+}
+
+SEMOverlay *ABCSEMOverlayCreate(FileLocation *location)
+{
+    return NodeCreate(SEMOverlay, location);
+}
+static void SEMMidiVoiceAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitMidiVoice(visitor, self);
+}
+
+static void SEMMidiVoiceDestroy(void *self)
+{
+}
+
+SEMMidiVoice *ABCSEMMidiVoiceCreate(FileLocation *location)
+{
+    return NodeCreate(SEMMidiVoice, location);
+}
+static void SEMPropagateAccidentalAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitPropagateAccidental(visitor, self);
+}
+
+static void SEMPropagateAccidentalDestroy(void *self)
+{
+}
+
+SEMPropagateAccidental *ABCSEMPropagateAccidentalCreate(FileLocation *location)
+{
+    return NodeCreate(SEMPropagateAccidental, location);
+}
--- parser/abc/ABCSEM.h
+++ parser/abc/ABCSEM.h
@@ -76,15 +76,20 @@ typedef struct _SEMRest {
     } length;
 } SEMRest;
 
+typedef enum {
+    RepeatStart,
+    RepeatEnd,
+    RepeatNth,
+} SEMRepeatType;
+
 typedef struct _SEMRepeat {
     Node node;
-    int type; // TODO
-    int number;
+    SEMRepeatType type;
+    int nth;
 } SEMRepeat;
 
 typedef struct _SEMBarLine {
     Node node;
-    int type; // TODO
 } SEMBarLine;
 
 typedef struct _SEMTie {
@@ -125,3 +130,61 @@ typedef struct _SEMPropagateAccidental {
     bool untilBar;
     bool allOctave;
 } SEMPropagateAccidental;
+
+
+typedef struct _SEMVisitor {
+    void (*visitFile)(void *self, SEMFile *sem);
+    void (*visitTune)(void *self, SEMTune *sem);
+    void (*visitTempo)(void *self, SEMTempo *sem);
+    void (*visitTime)(void *self, SEMTime *sem);
+    void (*visitKey)(void *self, SEMKey *sem);
+    void (*visitUnitNoteLength)(void *self, SEMUnitNoteLength *sem);
+    void (*visitPart)(void *self, SEMPart *sem);
+    void (*visitVoice)(void *self, SEMVoice *sem);
+    void (*visitNote)(void *self, SEMNote *sem);
+    void (*visitBrokenRhythm)(void *self, SEMBrokenRhythm *sem);
+    void (*visitRest)(void *self, SEMRest *sem);
+    void (*visitRepeat)(void *self, SEMRepeat *sem);
+    void (*visitBarLine)(void *self, SEMBarLine *sem);
+    void (*visitTie)(void *self, SEMTie *sem);
+    void (*visitGraceNote)(void *self, SEMGraceNote *sem);
+    void (*visitTuplet)(void *self, SEMTuplet *sem);
+    void (*visitChord)(void *self, SEMChord *sem);
+    void (*visitOverlay)(void *self, SEMOverlay *sem);
+    void (*visitMidiVoice)(void *self, SEMMidiVoice *sem);
+    void (*visitPropagateAccidental)(void *self, SEMPropagateAccidental *sem);
+} SEMVisitor;
+
+
+extern SEMFile *ABCSEMFileCreate(FileLocation *location);
+extern SEMTune *ABCSEMTuneCreate(FileLocation *location);
+extern SEMTempo *ABCSEMTempoCreate(FileLocation *location);
+extern SEMTime *ABCSEMTimeCreate(FileLocation *location);
+extern SEMKey *ABCSEMKeyCreate(FileLocation *location);
+extern SEMUnitNoteLength *ABCSEMUnitNoteLengthCreate(FileLocation *location);
+extern SEMPart *ABCSEMPartCreate(FileLocation *location);
+extern SEMVoice *ABCSEMVoiceCreate(FileLocation *location);
+extern SEMNote *ABCSEMNoteCreate(FileLocation *location);
+extern SEMBrokenRhythm *ABCSEMBrokenRhythmCreate(FileLocation *location);
+extern SEMRest *ABCSEMRestCreate(FileLocation *location);
+extern SEMRepeat *ABCSEMRepeatCreate(FileLocation *location);
+extern SEMBarLine *ABCSEMBarLineCreate(FileLocation *location);
+extern SEMTie *ABCSEMTieCreate(FileLocation *location);
+extern SEMGraceNote *ABCSEMGraceNoteCreate(FileLocation *location);
+extern SEMTuplet *ABCSEMTupletCreate(FileLocation *location);
+extern SEMChord *ABCSEMChordCreate(FileLocation *location);
+extern SEMOverlay *ABCSEMOverlayCreate(FileLocation *location);
+extern SEMMidiVoice *ABCSEMMidiVoiceCreate(FileLocation *location);
+extern SEMPropagateAccidental *ABCSEMPropagateAccidentalCreate(FileLocation *location);
+
+
+static inline const char *SEMRepeatType2String(SEMRepeatType type)
+{
+#define CASE(type) case type: return #type;
+    switch (type) {
+    CASE(RepeatStart);
+    CASE(RepeatEnd);
+    CASE(RepeatNth);
+    }
+#define undef
+}
