46398aa359419e6a89fd3671e852a9a696600ac0
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-01-02 20:18:31 +0900

    fix preprocessor settings

--- lib/NALog.c
+++ lib/NALog.c
@@ -4,7 +4,11 @@ const int NALogLevelSilent = 0;
 const int NALogLevelDebug = 1;
 const int NALogLevelTrace = 2;
 
+#ifdef __TEST__
+int NALogLevel = NALogLevelDebug;
+#else
 int NALogLevel = NALogLevelSilent;
+#endif
 FILE *NALogStream = NULL;
 
 __attribute__((constructor))
