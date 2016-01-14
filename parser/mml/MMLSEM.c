9c4dde92ed15f85d1f87dfce1bf440d1c6163f9b
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-01-14 20:53:30 +0900

    semantics type definition

--- parser/mml/MMLSEM.c
+++ parser/mml/MMLSEM.c
@@ -18,3 +18,434 @@ SEMList *MMLSEMListCreate(FileLocation *location)
     self->node.children = NAArrayCreate(4, NULL);
     return self;
 }
+
+static void SEMTimebaseAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTimebase(visitor, self);
+}
+
+static void SEMTimebaseDestroy(void *self)
+{
+}
+
+SEMTimebase *MMLSEMTimebaseCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTimebase, location);
+}
+
+static void SEMTitleAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTitle(visitor, self);
+}
+
+static void SEMTitleDestroy(void *_self)
+{
+    SEMTitle *self = _self;
+    free(self->title);
+}
+
+SEMTitle *MMLSEMTitleCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTitle, location);
+}
+
+static void SEMCopyrightAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitCopyright(visitor, self);
+}
+
+static void SEMCopyrightDestroy(void *_self)
+{
+    SEMCopyright *self = _self;
+    free(self->text);
+}
+
+SEMCopyright *MMLSEMCopyrightCreate(FileLocation *location)
+{
+    return NodeCreate(SEMCopyright, location);
+}
+
+static void SEMMarkerAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitMarker(visitor, self);
+}
+
+static void SEMMarkerDestroy(void *_self)
+{
+    SEMMarker *self = _self;
+    free(self->text);
+}
+
+SEMMarker *MMLSEMMarkerCreate(FileLocation *location)
+{
+    return NodeCreate(SEMMarker, location);
+}
+
+static void SEMVelocityReverseAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitVelocityReverse(visitor, self);
+}
+
+static void SEMVelocityReverseDestroy(void *self)
+{
+}
+
+SEMVelocityReverse *MMLSEMVelocityReverseCreate(FileLocation *location)
+{
+    return NodeCreate(SEMVelocityReverse, location);
+}
+
+
+static void SEMOctaveReverseAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitOctaveReverse(visitor, self);
+}
+
+static void SEMOctaveReverseDestroy(void *self)
+{
+}
+
+SEMOctaveReverse *MMLSEMOctaveReverseCreate(FileLocation *location)
+{
+    return NodeCreate(SEMOctaveReverse, location);
+}
+
+static void SEMChannelAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitChannel(visitor, self);
+}
+
+static void SEMChannelDestroy(void *self)
+{
+}
+
+SEMChannel *MMLSEMChannelCreate(FileLocation *location)
+{
+    return NodeCreate(SEMChannel, location);
+}
+
+static void SEMSynthAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitSynth(visitor, self);
+}
+
+static void SEMSynthDestroy(void *_self)
+{
+    SEMSynth *self = _self;
+    free(self->name);
+}
+
+SEMSynth *MMLSEMSynthCreate(FileLocation *location)
+{
+    return NodeCreate(SEMSynth, location);
+}
+
+static void SEMBankSelectAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitBankSelect(visitor, self);
+}
+
+static void SEMBankSelectDestroy(void *self)
+{
+}
+
+SEMBankSelect *MMLSEMBankSelectCreate(FileLocation *location)
+{
+    return NodeCreate(SEMBankSelect, location);
+}
+
+static void SEMProgramChangeAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitProgramChange(visitor, self);
+}
+
+static void SEMProgramChangeDestroy(void *self)
+{
+}
+
+SEMProgramChange *MMLSEMProgramChangeCreate(FileLocation *location)
+{
+    return NodeCreate(SEMProgramChange, location);
+}
+
+static void SEMVolumeAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitVolume(visitor, self);
+}
+
+static void SEMVolumeDestroy(void *self)
+{
+}
+
+SEMVolume *MMLSEMVolumeCreate(FileLocation *location)
+{
+    return NodeCreate(SEMVolume, location);
+}
+
+static void SEMChorusAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitChorus(visitor, self);
+}
+
+static void SEMChorusDestroy(void *self)
+{
+}
+
+SEMChorus *MMLSEMChorusCreate(FileLocation *location)
+{
+    return NodeCreate(SEMChorus, location);
+}
+
+static void SEMReverbAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitReverb(visitor, self);
+}
+
+static void SEMReverbDestroy(void *self)
+{
+}
+
+SEMReverb *MMLSEMReverbCreate(FileLocation *location)
+{
+    return NodeCreate(SEMReverb, location);
+}
+
+static void SEMExpressionAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitExpression(visitor, self);
+}
+
+static void SEMExpressionDestroy(void *self)
+{
+}
+
+SEMExpression *MMLSEMExpressionCreate(FileLocation *location)
+{
+    return NodeCreate(SEMExpression, location);
+}
+
+static void SEMPanAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitPan(visitor, self);
+}
+
+static void SEMPanDestroy(void *self)
+{
+}
+
+SEMPan *MMLSEMPanCreate(FileLocation *location)
+{
+    return NodeCreate(SEMPan, location);
+}
+
+static void SEMDetuneAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitDetune(visitor, self);
+}
+
+static void SEMDetuneDestroy(void *self)
+{
+}
+
+SEMDetune *MMLSEMDetuneCreate(FileLocation *location)
+{
+    return NodeCreate(SEMDetune, location);
+}
+
+static void SEMTempoAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTempo(visitor, self);
+}
+
+static void SEMTempoDestroy(void *self)
+{
+}
+
+SEMTempo *MMLSEMTempoCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTempo, location);
+}
+
+static void SEMNoteAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitNote(visitor, self);
+}
+
+static void SEMNoteDestroy(void *_self)
+{
+    SEMNote *self = _self;
+    free(self->noteString);
+}
+
+SEMNote *MMLSEMNoteCreate(FileLocation *location)
+{
+    return NodeCreate(SEMNote, location);
+}
+
+static void SEMRestAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitRest(visitor, self);
+}
+
+static void SEMRestDestroy(void *self)
+{
+}
+
+SEMRest *MMLSEMRestCreate(FileLocation *location)
+{
+    return NodeCreate(SEMRest, location);
+}
+
+static void SEMOctaveAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitOctave(visitor, self);
+}
+
+static void SEMOctaveDestroy(void *self)
+{
+}
+
+SEMOctave *MMLSEMOctaveCreate(FileLocation *location)
+{
+    return NodeCreate(SEMOctave, location);
+}
+
+static void SEMTranseposeAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTransepose(visitor, self);
+}
+
+static void SEMTranseposeDestroy(void *self)
+{
+}
+
+SEMTransepose *MMLSEMTranseposeCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTransepose, location);
+}
+
+static void SEMTieAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTie(visitor, self);
+}
+
+static void SEMTieDestroy(void *self)
+{
+}
+
+SEMTie *MMLSEMTieCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTie, location);
+}
+
+static void SEMLengthAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitLength(visitor, self);
+}
+
+static void SEMLengthDestroy(void *self)
+{
+}
+
+SEMLength *MMLSEMLengthCreate(FileLocation *location)
+{
+    return NodeCreate(SEMLength, location);
+}
+
+static void SEMGatetimeAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitGatetime(visitor, self);
+}
+
+static void SEMGatetimeDestroy(void *self)
+{
+}
+
+SEMGatetime *MMLSEMGatetimeCreate(FileLocation *location)
+{
+    return NodeCreate(SEMGatetime, location);
+}
+
+static void SEMVelocityAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitVelocity(visitor, self);
+}
+
+static void SEMVelocityDestroy(void *self)
+{
+}
+
+SEMVelocity *MMLSEMVelocityCreate(FileLocation *location)
+{
+    return NodeCreate(SEMVelocity, location);
+}
+
+static void SEMTupletAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTuplet(visitor, self);
+}
+
+static void SEMTupletDestroy(void *self)
+{
+}
+
+SEMTuplet *MMLSEMTupletCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTuplet, location);
+}
+
+static void SEMTrackChangeAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitTrackChange(visitor, self);
+}
+
+static void SEMTrackChangeDestroy(void *self)
+{
+}
+
+SEMTrackChange *MMLSEMTrackChangeCreate(FileLocation *location)
+{
+    return NodeCreate(SEMTrackChange, location);
+}
+
+static void SEMRepeatAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitRepeat(visitor, self);
+}
+
+static void SEMRepeatDestroy(void *self)
+{
+}
+
+SEMRepeat *MMLSEMRepeatCreate(FileLocation *location)
+{
+    return NodeCreate(SEMRepeat, location);
+}
+
+static void SEMRepeatBreakAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitRepeatBreak(visitor, self);
+}
+
+static void SEMRepeatBreakDestroy(void *self)
+{
+}
+
+SEMRepeatBreak *MMLSEMRepeatBreakCreate(FileLocation *location)
+{
+    return NodeCreate(SEMRepeatBreak, location);
+}
+
+static void SEMChordAccept(void *self, void *visitor)
+{
+    ((SEMVisitor *)visitor)->visitChord(visitor, self);
+}
+
+static void SEMChordDestroy(void *self)
+{
+}
+
+SEMChord *MMLSEMChordCreate(FileLocation *location)
+{
+    return NodeCreate(SEMChord, location);
+}
--- parser/mml/MMLSEM.h
+++ parser/mml/MMLSEM.h
@@ -2,14 +2,232 @@
 
 #include "Node.h"
 #include "NoteTable.h"
-#include "NAArray.h"
 
 typedef struct _SEMList {
     Node node;
 } SEMList;
 
+typedef struct _SEMTimebase {
+    Node node;
+    int timebase;
+} SEMTimebase;
+
+typedef struct _SEMTitle {
+    Node node;
+    char *title;
+} SEMTitle;
+
+typedef struct _SEMCopyright {
+    Node node;
+    char *text;
+} SEMCopyright;
+
+typedef struct _SEMMarker {
+    Node node;
+    char *text;
+} SEMMarker;
+
+typedef struct _SEMVelocityReverse {
+    Node node;
+} SEMVelocityReverse;
+
+typedef struct _SEMOctaveReverse {
+    Node node;
+} SEMOctaveReverse;
+
+typedef struct _SEMChannel {
+    Node node;
+    int number;
+} SEMChannel;
+
+typedef struct _SEMSynth {
+    Node node;
+    char *name;
+} SEMSynth;
+
+typedef struct _SEMBankSelect {
+    Node node;
+    int msb;
+    int lsb;
+} SEMBankSelect;
+
+typedef struct _SEMProgramChange {
+    Node node;
+    int programNo;
+} SEMProgramChange;
+
+typedef struct _SEMVolume {
+    Node node;
+    int value;
+} SEMVolume;
+
+typedef struct _SEMChorus {
+    Node node;
+    int value;
+} SEMChorus;
+
+typedef struct _SEMReverb {
+    Node node;
+    int value;
+} SEMReverb;
+
+typedef struct _SEMExpression {
+    Node node;
+    int value;
+} SEMExpression;
+
+typedef struct _SEMPan {
+    Node node;
+    int value;
+} SEMPan;
+
+typedef struct _SEMDetune {
+    Node node;
+    int value;
+} SEMDetune;
+
+typedef struct _SEMTempo {
+    Node node;
+    float tempo;
+} SEMTempo;
+
+typedef struct _NoteLength {
+    int length;
+    int dotCount;
+    int gatetime;
+} NoteLength;
+
+typedef struct _SEMNote {
+    Node node;
+    BaseNote baseNote;
+    Accidental accidental;
+    NoteLength length;
+    char *noteString;
+} SEMNote;
+
+typedef struct _SEMRest {
+    Node node;
+    NoteLength length;
+} SEMRest;
+
+typedef struct _SEMOctave {
+    Node node;
+    char direction;
+    int value;
+} SEMOctave;
+
+typedef struct _SEMTranspose {
+    Node node;
+    bool relative;
+    int value;
+} SEMTransepose;
+
+typedef struct _SEMTie {
+    Node node;
+} SEMTie;
+
+typedef struct _SEMLength {
+    Node node;
+    int length;
+} SEMLength;
+
+typedef struct _SEMGatetime {
+    Node node;
+    bool absolute;
+    int value;
+} SEMGatetime;
+
+typedef struct _SEMVelocity {
+    Node node;
+    char direction;
+    bool absolute;
+    int value;
+} SEMVelocity;
+
+typedef struct _SEMTuplet {
+    Node node;
+    NoteLength length;
+} SEMTuplet;
+
+typedef struct _SEMTrackChange {
+    Node node;
+} SEMTrackChange;
+
+typedef struct _SEMRepeat {
+    Node node;
+    int times;
+} SEMRepeat;
+
+typedef struct _SEMRepeatBreak {
+    Node node;
+} SEMRepeatBreak;
+
+typedef struct _SEMChord {
+    Node node;
+} SEMChord;
+
 typedef struct _SEMVisitor {
     void (*visitList)(void *self, SEMList *sem);
+    void (*visitTimebase)(void *self, SEMTimebase *sem);
+    void (*visitTitle)(void *self, SEMTitle *sem);
+    void (*visitCopyright)(void *self, SEMCopyright *sem);
+    void (*visitMarker)(void *self, SEMMarker *sem);
+    void (*visitVelocityReverse)(void *self, SEMVelocityReverse *sem);
+    void (*visitOctaveReverse)(void *self, SEMOctaveReverse *sem);
+    void (*visitChannel)(void *self, SEMChannel *sem);
+    void (*visitSynth)(void *self, SEMSynth *sem);
+    void (*visitBankSelect)(void *self, SEMBankSelect *sem);
+    void (*visitProgramChange)(void *self, SEMProgramChange *sem);
+    void (*visitVolume)(void *self, SEMVolume *sem);
+    void (*visitChorus)(void *self, SEMChorus *sem);
+    void (*visitReverb)(void *self, SEMReverb *sem);
+    void (*visitExpression)(void *self, SEMExpression *sem);
+    void (*visitPan)(void *self, SEMPan *sem);
+    void (*visitDetune)(void *self, SEMDetune *sem);
+    void (*visitTempo)(void *self, SEMTempo *sem);
+    void (*visitNote)(void *self, SEMNote *sem);
+    void (*visitRest)(void *self, SEMRest *sem);
+    void (*visitOctave)(void *self, SEMOctave *sem);
+    void (*visitTransepose)(void *self, SEMTransepose *sem);
+    void (*visitTie)(void *self, SEMTie *sem);
+    void (*visitLength)(void *self, SEMLength *sem);
+    void (*visitGatetime)(void *self, SEMGatetime *sem);
+    void (*visitVelocity)(void *self, SEMVelocity *sem);
+    void (*visitTuplet)(void *self, SEMTuplet *sem);
+    void (*visitTrackChange)(void *self, SEMTrackChange *sem);
+    void (*visitRepeat)(void *self, SEMRepeat *sem);
+    void (*visitRepeatBreak)(void *self, SEMRepeatBreak *sem);
+    void (*visitChord)(void *self, SEMChord *sem);
 } SEMVisitor;
 
 extern SEMList *MMLSEMListCreate(FileLocation *location);
+extern SEMTimebase *MMLSEMTimebaseCreate(FileLocation *location);
+extern SEMTitle *MMLSEMTitleCreate(FileLocation *location);
+extern SEMCopyright *MMLSEMCopyrightCreate(FileLocation *location);
+extern SEMMarker *MMLSEMMarkerCreate(FileLocation *location);
+extern SEMVelocityReverse *MMLSEMVelocityReverseCreate(FileLocation *location);
+extern SEMOctaveReverse *MMLSEMOctaveReverseCreate(FileLocation *location);
+extern SEMChannel *MMLSEMChannelCreate(FileLocation *location);
+extern SEMSynth *MMLSEMSynthCreate(FileLocation *location);
+extern SEMBankSelect *MMLSEMBankSelectCreate(FileLocation *location);
+extern SEMProgramChange *MMLSEMProgramChangeCreate(FileLocation *location);
+extern SEMVolume *MMLSEMVolumeCreate(FileLocation *location);
+extern SEMChorus *MMLSEMChorusCreate(FileLocation *location);
+extern SEMReverb *MMLSEMReverbCreate(FileLocation *location);
+extern SEMExpression *MMLSEMExpressionCreate(FileLocation *location);
+extern SEMPan *MMLSEMPanCreate(FileLocation *location);
+extern SEMDetune *MMLSEMDetuneCreate(FileLocation *location);
+extern SEMTempo *MMLSEMTempoCreate(FileLocation *location);
+extern SEMNote *MMLSEMNoteCreate(FileLocation *location);
+extern SEMRest *MMLSEMRestCreate(FileLocation *location);
+extern SEMOctave *MMLSEMOctaveCreate(FileLocation *location);
+extern SEMTransepose *MMLSEMTranseposeCreate(FileLocation *location);
+extern SEMTie *MMLSEMTieCreate(FileLocation *location);
+extern SEMLength *MMLSEMLengthCreate(FileLocation *location);
+extern SEMGatetime *MMLSEMGatetimeCreate(FileLocation *location);
+extern SEMVelocity *MMLSEMVelocityCreate(FileLocation *location);
+extern SEMTuplet *MMLSEMTupletCreate(FileLocation *location);
+extern SEMTrackChange *MMLSEMTrackChangeCreate(FileLocation *location);
+extern SEMRepeat *MMLSEMRepeatCreate(FileLocation *location);
+extern SEMRepeatBreak *MMLSEMRepeatBreakCreate(FileLocation *location);
+extern SEMChord *MMLSEMChordCreate(FileLocation *location);
