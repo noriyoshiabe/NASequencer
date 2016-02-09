92aedbc948790e31d429299a36e5177cb4d3f19d
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-02-09 17:31:35 +0900

    connect ui action for welcome window

--- cocoa/NAMIDI/WelcomeWindowController.m
+++ cocoa/NAMIDI/WelcomeWindowController.m
@@ -26,4 +26,45 @@
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 }
 
+- (IBAction)closeButtonTapped:(id)sender
+{
+    NSLog(@"%s", __func__);
+    [self close];
+}
+
+- (IBAction)gettingStartedButtonTapped:(id)sender
+{
+    NSLog(@"%s", __func__);
+}
+
+- (IBAction)listenToExampleButtonTapped:(id)sender
+{
+    NSLog(@"%s", __func__);
+}
+
+- (IBAction)createNewDocumentButtonTapped:(id)sender
+{
+    NSLog(@"%s", __func__);
+}
+
+- (IBAction)helpButtonTapped:(id)sender
+{
+    NSLog(@"%s", __func__);
+}
+
+- (IBAction)preferenceButtonTapped:(id)sender
+{
+    NSLog(@"%s", __func__);
+}
+
+- (IBAction)openOtherDocumentButtonTapped:(id)sender
+{
+    NSLog(@"%s", __func__);
+}
+
+- (IBAction)showWhelcomeWhenStartsToggled:(NSButton *)sender
+{
+    NSLog(@"%s %s", NSOnState == sender.state ? "ON" : "OFF", __func__);
+}
+
 @end
