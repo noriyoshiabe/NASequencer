79f813391f8527e40fa556ac460e8eb141bfd4dc
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-02-11 13:46:49 +0900

    wip organize class composition

--- cocoa/NAMIDI/Welcome/WelcomeWindowController.m
+++ cocoa/NAMIDI/Welcome/WelcomeWindowController.m
@@ -7,12 +7,43 @@
 //
 
 #import "WelcomeWindowController.h"
-#import "WelcomeView.h"
 #import "Application.h"
 #import "ApplicationController.h"
 
-@interface WelcomeWindowController () <WelcomeViewDelegate>
-@property (weak) IBOutlet WelcomeView *welcomeView;
+#include <pwd.h>
+
+@class RecentTableView;
+@protocol RecentTableViewDelegate <NSTableViewDelegate>
+- (void)tableViewDidKeyDownEnter:(RecentTableView *)tableView;
+@end
+
+@interface RecentTableView : NSTableView
+@end
+
+@implementation RecentTableView
+
+- (void)keyDown:(NSEvent *)theEvent
+{
+    unichar key = [[theEvent charactersIgnoringModifiers] characterAtIndex:0];
+    if (NSCarriageReturnCharacter == key && -1 != self.selectedRow) {
+        [((id<RecentTableViewDelegate>)self.delegate) tableViewDidKeyDownEnter:self];
+    }
+    else {
+        [super keyDown:theEvent];
+    }
+}
+
+@end
+
+@interface WelcomeViewRecentTableCell : NSTableCellView
+@property (weak) IBOutlet NSTextField *detailTextField;
+@end
+
+@implementation WelcomeViewRecentTableCell
+@end
+
+@interface WelcomeWindowController () <RecentTableViewDelegate, NSTableViewDataSource>
+@property (weak) IBOutlet RecentTableView *recentTableView;
 @end
 
 @implementation WelcomeWindowController
@@ -27,56 +58,92 @@
     [super windowDidLoad];
     self.windowFrameAutosaveName = @"WelcomeWindowFrame";
     
-    _welcomeView.delegate = self;
-    _welcomeView.recentFiles = [NSDocumentController sharedDocumentController].recentDocumentURLs;
+    _recentTableView.delegate = self;
+    _recentTableView.dataSource = self;
+    
+    [_recentTableView reloadData];
+    
+    if (0 < self.recentDocumentURLs.count) {
+        [_recentTableView selectRowIndexes:[NSIndexSet indexSetWithIndex:0] byExtendingSelection:NO];
+    }
 }
 
-#pragma mark WelcomeViewDelegate
+- (NSArray *)recentDocumentURLs
+{
+    return [NSDocumentController sharedDocumentController].recentDocumentURLs;
+}
 
-- (void)welcomeView:(WelcomeView *)view closeButtonTapped:(id)sender
+- (IBAction)closeButtonTapped:(id)sender
 {
     [self close];
 }
 
-- (void)welcomeView:(WelcomeView *)view gettingStartedButtonTapped:(id)sender
+- (IBAction)gettingStartedButtonTapped:(id)sender
 {
     [[ApplicationController sharedInstance] showGettingStartedWindow];
 }
 
-- (void)welcomeView:(WelcomeView *)view listenToExampleButtonTapped:(id)sender
+- (IBAction)listenToExampleButtonTapped:(id)sender
 {
     NSLog(@"%s", __func__);
 }
 
-- (void)welcomeView:(WelcomeView *)view createNewDocumentButtonTapped:(id)sender
+- (IBAction)createNewDocumentButtonTapped:(id)sender
 {
     [[Application sharedApplication] createDocument];
 }
 
-- (void)welcomeView:(WelcomeView *)view helpButtonTapped:(id)sender
+- (IBAction)helpButtonTapped:(id)sender
 {
     NSString *bookName = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleHelpBookName"];
     [[NSHelpManager sharedHelpManager] openHelpAnchor:@"top" inBook:bookName];
 }
 
-- (void)welcomeView:(WelcomeView *)view preferenceButtonTapped:(id)sender
+- (IBAction)preferenceButtonTapped:(id)sender
 {
     [[ApplicationController sharedInstance] showPreferenceWindow];
 }
 
-- (void)welcomeView:(WelcomeView *)view openOtherDocumentButtonTapped:(id)sender
+- (IBAction)openOtherDocumentButtonTapped:(id)sender
 {
     [[Application sharedApplication] openDocument];
 }
 
-- (void)welcomeView:(WelcomeView *)view showWelcomeWhenStartsToggled:(NSButton *)sender
+- (IBAction)showWelcomeWhenStartsToggled:(NSButton *)sender
 {
     NSLog(@"%s %s", NSOnState == sender.state ? "ON" : "OFF", __func__);
 }
 
-- (void)welcomeView:(WelcomeView *)view recentTableViewSelectionChanged:(id)sender selectedRow:(NSInteger)row
+- (IBAction)recentTableViewSelectionChanged:(id)sender
 {
-    [[Application sharedApplication] openDocumentWithContentsOfURL:_welcomeView.recentFiles[row]];
+    [[Application sharedApplication] openDocumentWithContentsOfURL:self.recentDocumentURLs[[_recentTableView selectedRow]]];
+}
+
+#pragma mark RecentTableViewDelegate
+
+- (void)tableViewDidKeyDownEnter:(RecentTableView *)tableView
+{
+    [[Application sharedApplication] openDocumentWithContentsOfURL:self.recentDocumentURLs[[_recentTableView selectedRow]]];
+}
+
+#pragma mark NSTableViewDelegate
+
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    NSURL *url = [self.recentDocumentURLs objectAtIndex:row];
+    
+    WelcomeViewRecentTableCell *cellView = [tableView makeViewWithIdentifier:tableColumn.identifier owner:self];
+    cellView.imageView.image = [[NSWorkspace sharedWorkspace] iconForFileType:url.lastPathComponent.pathExtension];
+    cellView.textField.stringValue = url.lastPathComponent;
+    cellView.detailTextField.stringValue = [url.path.stringByDeletingLastPathComponent stringByReplacingOccurrencesOfString:[NSString stringWithCString:getpwuid(getuid())->pw_dir encoding:NSUTF8StringEncoding] withString:@"~"];
+    return cellView;
+}
+
+#pragma mark NSTableViewDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return self.recentDocumentURLs.count;
 }
 
 @end
