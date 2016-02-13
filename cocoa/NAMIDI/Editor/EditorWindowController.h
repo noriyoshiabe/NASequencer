3aeda8480b74b66579f33660e27cd9d51756d671
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-02-13 18:29:03 +0900

    editor window controller

---
+++ cocoa/NAMIDI/Editor/EditorWindowController.h
@@ -0,0 +1,13 @@
+//
+//  EditorWindowController.h
+//  NAMIDI
+//
+//  Created by abechan on 2/13/16.
+//  Copyright © 2016 Noriyoshi Abe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EditorWindowController : NSWindowController
+
+@end
---
+++ cocoa/NAMIDI/Editor/EditorWindowController.m
@@ -0,0 +1,29 @@
+//
+//  EditorWindowController.m
+//  NAMIDI
+//
+//  Created by abechan on 2/13/16.
+//  Copyright © 2016 Noriyoshi Abe. All rights reserved.
+//
+
+#import "EditorWindowController.h"
+
+@interface EditorWindowController ()
+
+@end
+
+@implementation EditorWindowController
+
+- (instancetype)init
+{
+    return [super initWithWindowNibName:@"EditorWindowController"];
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
