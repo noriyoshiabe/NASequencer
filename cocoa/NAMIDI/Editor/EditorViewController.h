f80730d8ec8e831ce8246d3eeb059c08f8e79ab9
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-02-14 00:43:10 +0900

    editor view controller

---
+++ cocoa/NAMIDI/Editor/EditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditorViewController.h
+//  NAMIDI
+//
+//  Created by abechan on 2/13/16.
+//  Copyright © 2016 Noriyoshi Abe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EditorViewController : NSViewController
+
+@end
---
+++ cocoa/NAMIDI/Editor/EditorViewController.m
@@ -0,0 +1,27 @@
+//
+//  EditorViewController.m
+//  NAMIDI
+//
+//  Created by abechan on 2/13/16.
+//  Copyright © 2016 Noriyoshi Abe. All rights reserved.
+//
+
+#import "EditorViewController.h"
+
+@interface EditorViewController ()
+
+@end
+
+@implementation EditorViewController
+
+- (instancetype)init
+{
+    return [super initWithNibName:@"EditorViewController" bundle:nil];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
