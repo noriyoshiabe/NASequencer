64b07d6356e72a935cc5ad42f10c784f60855c79
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-02-21 19:44:54 +0900

    detail view frame

--- cocoa/NAMIDI/Main/DetailViewController.m
+++ cocoa/NAMIDI/Main/DetailViewController.m
@@ -8,16 +8,88 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
+#define VERTICAL_SPLIT_LEFT_MAX 400
+#define VERTICAL_SPLIT_LEFT_MIN 240
 
+#define HORIZONTAL_SPLIT_BOTTOM_MAX 160
+#define HORIZONTAL_SPLIT_BOTTOM_MIN 40
+
+@interface DetailViewController () <NSSplitViewDelegate>
+@property (weak) IBOutlet NSSplitView *verticalSplitView;
+@property (weak) IBOutlet NSSplitView *horizontalSplitView;
+@property (weak) IBOutlet NSView *selectionView;
+@property (weak) IBOutlet NSView *eventListView;
+@property (weak) IBOutlet NSView *measureView;
+@property (weak) IBOutlet NSView *conductorView;
+@property (weak) IBOutlet NSView *pianoRollView;
+@property (weak) IBOutlet NSView *velocityView;
 @end
 
 @implementation DetailViewController
 
+- (void)awakeFromNib
+{
+    _selectionView.wantsLayer = YES;
+    _eventListView.wantsLayer = YES;
+    _measureView.wantsLayer = YES;
+    _conductorView.wantsLayer = YES;
+    _pianoRollView.wantsLayer = YES;
+    _velocityView.wantsLayer = YES;
+    
+    _selectionView.layer.backgroundColor = [NSColor redColor].CGColor;
+    _eventListView.layer.backgroundColor = [NSColor blueColor].CGColor;
+    _measureView.layer.backgroundColor = [NSColor greenColor].CGColor;
+    _conductorView.layer.backgroundColor = [NSColor yellowColor].CGColor;
+    _pianoRollView.layer.backgroundColor = [NSColor magentaColor].CGColor;
+    _velocityView.layer.backgroundColor = [NSColor cyanColor].CGColor;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do view setup here.
+    
+    _verticalSplitView.delegate = self;
+    _horizontalSplitView.delegate = self;
+}
+
+- (void)viewDidAppear
+{
+    [super viewDidAppear];
+    
+    [_verticalSplitView setPosition:VERTICAL_SPLIT_LEFT_MAX ofDividerAtIndex:0];
+    [_horizontalSplitView setPosition:CGRectGetHeight(self.view.frame) - HORIZONTAL_SPLIT_BOTTOM_MAX ofDividerAtIndex:0];
+}
+
+#pragma mark NSSplitViewDelegate
+
+- (CGFloat)splitView:(NSSplitView *)splitView constrainMinCoordinate:(CGFloat)proposedMinimumPosition ofSubviewAt:(NSInteger)dividerIndex
+{
+    if (splitView.vertical) {
+        return VERTICAL_SPLIT_LEFT_MIN;
+    }
+    else {
+        return CGRectGetHeight(self.view.frame) - HORIZONTAL_SPLIT_BOTTOM_MAX;
+    }
+}
+
+- (CGFloat)splitView:(NSSplitView *)splitView constrainMaxCoordinate:(CGFloat)proposedMaximumPosition ofSubviewAt:(NSInteger)dividerIndex
+{
+    if (splitView.vertical) {
+        return VERTICAL_SPLIT_LEFT_MAX;
+    }
+    else {
+        return CGRectGetHeight(self.view.frame) - HORIZONTAL_SPLIT_BOTTOM_MIN;
+    }
+}
+
+- (BOOL)splitView:(NSSplitView *)splitView shouldAdjustSizeOfSubview:(NSView *)view
+{
+    if (splitView.vertical) {
+        return splitView.subviews[1] == view;
+    }
+    else {
+        return splitView.subviews[0] == view;
+    }
 }
 
 @end
