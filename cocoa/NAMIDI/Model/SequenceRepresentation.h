c87227f3a23bfaef618e5373cf7041c73613c273
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-02-25 04:36:47 +0900

    sequence representation

---
+++ cocoa/NAMIDI/Model/SequenceRepresentation.h
@@ -0,0 +1,35 @@
+//
+//  SequenceRepresentation.h
+//  NAMIDI
+//
+//  Created by abechan on 2/25/16.
+//  Copyright © 2016 Noriyoshi Abe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MidiEventRepresentation.h"
+#import "Sequence.h"
+
+typedef struct _NoteRange {
+    int16_t low;
+    int16_t high;
+} NoteRange;
+
+@interface ChannelRepresentation : NSObject
+@property (readonly, nonatomic) int number;
+@property (readonly, nonatomic) BOOL exsit;
+@property (readonly, nonatomic) NSArray *events;
+@property (readonly, nonatomic) NoteRange noteRange;
+@end
+
+@interface SequenceRepresentation : NSObject
+@property (readonly, nonatomic) NSString *title;
+@property (readonly, nonatomic) int32_t length;
+@property (readonly, nonatomic) NSArray<MidiEventRepresentation *> *events;
+@property (readonly, nonatomic) NSArray<MidiEventRepresentation *> *eventsOfConductorTrack;
+@property (readonly, nonatomic) NSArray<ChannelRepresentation *> *channels;
+- (instancetype)initWithSequence:(Sequence *)sequence;
+- (TimeSign)timeSignByTick:(int)tick;
+- (Location)locationByTick:(int)tick;
+- (int)tickByLocation:(Location)location;
+@end
---
+++ cocoa/NAMIDI/Model/SequenceRepresentation.m
@@ -0,0 +1,127 @@
+//
+//  SequenceRepresentation.m
+//  NAMIDI
+//
+//  Created by abechan on 2/25/16.
+//  Copyright © 2016 Noriyoshi Abe. All rights reserved.
+//
+
+#import "SequenceRepresentation.h"
+
+@interface ChannelRepresentation () {
+    NSMutableArray *_events;
+}
+
+@property (readwrite, nonatomic) int number;
+@property (readwrite, nonatomic) NoteRange noteRange;
+- (NSMutableArray *)_events;
+- (void)addEvent:(MidiEventRepresentation *)event;
+@end
+
+@implementation ChannelRepresentation
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _events = [NSMutableArray array];
+        _noteRange.low = 127;
+        _noteRange.high = 0;
+    }
+    return self;
+}
+
+- (BOOL)exsit
+{
+    return 0 < [_events count];
+}
+
+- (NSMutableArray *)_events
+{
+    return (NSMutableArray *)_events;
+}
+
+- (void)addEvent:(MidiEventRepresentation *)event
+{
+    [_events addObject:event];
+    
+    if (MidiEventTypeNote == event.raw->type) {
+        NoteEvent *note = (NoteEvent *)event.raw;
+        _noteRange.low = MIN(note->noteNo, _noteRange.low);
+        _noteRange.high = MAX(note->noteNo, _noteRange.high);
+    }
+}
+
+@end
+
+@interface SequenceRepresentation () {
+    Sequence *_sequence;
+    NSMutableArray *_events;
+    NSMutableArray *_eventsOfConductorTrack;
+    NSMutableArray *_channels;
+}
+
+@end
+
+@implementation SequenceRepresentation
+
+- (instancetype)initWithSequence:(Sequence *)sequence
+{
+    self = [super init];
+    if (self) {
+        _sequence = sequence;
+        _events = [NSMutableArray array];
+        _eventsOfConductorTrack = [NSMutableArray array];
+        _channels = [NSMutableArray array];
+        
+        for (int i = 1; i <= 16; ++i) {
+            ChannelRepresentation *channel = [[ChannelRepresentation alloc] init];
+            channel.number = i;
+            [_channels addObject:channel];
+        }
+        
+        NAIterator *iterator = NAArrayGetIterator(sequence->events);
+        while (iterator->hasNext(iterator)) {
+            MidiEvent *raw = iterator->next(iterator);
+            MidiEventRepresentation *event = [[MidiEventRepresentation alloc] initWithMidiEvent:raw];
+            
+            int channel = event.channel;
+            if (0 == channel) {
+                [_eventsOfConductorTrack addObject:event];
+            }
+            else {
+                [_channels[channel - 1] addEvent:event];
+            }
+            
+            [_events addObject:event];
+        }
+    }
+    return self;
+}
+
+- (NSString *)title
+{
+    return [NSString stringWithUTF8String:_sequence->title];
+}
+
+- (int32_t)length
+{
+    return TimeTableLength(_sequence->timeTable);
+}
+
+- (TimeSign)timeSignByTick:(int)tick
+{
+    return TimeTableTimeSignOnTick(_sequence->timeTable, tick);
+}
+
+- (Location)locationByTick:(int)tick
+{
+    return TimeTableTick2Location(_sequence->timeTable, tick);
+}
+
+- (int)tickByLocation:(Location)location
+{
+    return TimeTableTickByLocation(_sequence->timeTable, location);
+}
+
+@end
