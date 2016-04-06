5c416321168d677da6be9fbb05e0e02950f78867
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-04-07 08:25:24 +0900

    fix wrong sec of location

--- cocoa/NAMIDI/Main/LocationView.m
+++ cocoa/NAMIDI/Main/LocationView.m
@@ -40,8 +40,8 @@
     int64_t usec = _player.usec;
     Location location = _player.location;
     
-    int min = (int)(usec / (1000 * 1000 * 60));
-    int sec = (int)(usec / (1000 * 1000));
+    int min = (int)((usec / (1000 * 1000)) / 60);
+    int sec = (int)((usec / (1000 * 1000)) % 60);
     int msec = (int)((usec / 1000) % 1000);
     
     _locationField.stringValue = [NSString stringWithFormat:@"%03d:%02d:%03d  %02d:%02d:%03d",
