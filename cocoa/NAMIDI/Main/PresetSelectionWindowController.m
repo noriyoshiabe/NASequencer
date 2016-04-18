68997438130031171de2102c661f446f8b9adc6b
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-04-18 21:39:35 +0900

    wip preset selection sheet

--- cocoa/NAMIDI/Main/PresetSelectionWindowController.m
+++ cocoa/NAMIDI/Main/PresetSelectionWindowController.m
@@ -8,12 +8,18 @@
 
 #import "PresetSelectionWindowController.h"
 
-@interface PresetSelectionWindowController () <NSWindowDelegate, NSTableViewDataSource, NSTableViewDelegate, MixerRepresentationObserver>
+@interface PresetTableView : NSTableView
+@property (weak, nonatomic) PresetSelectionWindowController *controller;
+@end
+
+@interface PresetSelectionWindowController () <NSWindowDelegate, NSTableViewDataSource, NSTableViewDelegate, MixerRepresentationObserver, NSWindowDelegate> {
+    int _octave;
+}
 @property (weak) IBOutlet NSTextField *channelField;
 @property (weak) IBOutlet NSTextField *synthesizerNameField;
 @property (weak) IBOutlet NSTextField *bankNoField;
 @property (weak) IBOutlet NSTextField *programNoField;
-@property (weak) IBOutlet NSTableView *presetTableView;
+@property (weak) IBOutlet PresetTableView *presetTableView;
 @end
 
 @implementation PresetSelectionWindowController
@@ -26,6 +32,8 @@
 - (void)windowDidLoad
 {
     [super windowDidLoad];
+    _presetTableView.controller = self;
+    _octave = 2;
 }
 
 - (void)setMixerChannel:(MixerChannelRepresentation *)mixerChannel
@@ -70,6 +78,11 @@
     [_presetTableView scrollRowToVisible:index];
 }
 
+- (void)updateOctaveLabel
+{
+    // TODO
+}
+
 - (NSString *)synthesizerName
 {
     return _mixerChannel.midiSourceDescription.name;
@@ -88,6 +101,117 @@
 - (IBAction)okButtonPressed:(id)sender
 {
     [self.window.sheetParent endSheet:self.window returnCode:NSModalResponseCancel];
+}
+
+- (void)cancelOperation:(id)sender
+{
+    [self.window.sheetParent endSheet:self.window returnCode:NSModalResponseCancel];
+}
+
+- (int)noteNo:(unichar)character
+{
+    const struct {
+        unichar character;
+        int baseNote;
+    } table[] = {
+        {'a', -4},
+        {'z', -3},
+        {'s', -2},
+        {'x', -1},
+        {'c', 0},
+        {'f', 1},
+        {'v', 2},
+        {'g', 3},
+        {'b', 4},
+        {'n', 5},
+        {'j', 6},
+        {'m', 7},
+        {'k', 8},
+        {',', 9},
+        {'l', 10},
+        {'.', 11},
+        {'/', 12},
+        
+        {'1', 8},
+        {'q', 9},
+        {'2', 10},
+        {'w', 11},
+        {'e', 12},
+        {'4', 13},
+        {'r', 14},
+        {'5', 15},
+        {'t', 16},
+        {'y', 17},
+        {'7', 18},
+        {'u', 19},
+        {'8', 20},
+        {'i', 21},
+        {'9', 22},
+        {'o', 23},
+        {'p', 24},
+    };
+    
+    for (int i = 0; i < sizeof(table)/sizeof(table[0]); ++i) {
+        if (table[i].character == character) {
+            int noteNo = table[i].baseNote + (_octave + 2) * 12;
+            if (0 <= noteNo && noteNo <= 127) {
+                return noteNo;
+            }
+            else {
+                return -1;
+            }
+        }
+    }
+    
+    return -1;
+}
+
+- (void)keyDown:(NSEvent *)theEvent
+{
+    unichar key = [theEvent.charactersIgnoringModifiers characterAtIndex:0];
+    switch (key) {
+        case NSCarriageReturnCharacter:
+            [self okButtonPressed:self];
+            break;
+        case NSLeftArrowFunctionKey:
+            if (-2 < _octave) {
+                --_octave;
+                [self updateOctaveLabel];
+            }
+            break;
+        case NSRightArrowFunctionKey:
+            if (8 > _octave) {
+                ++_octave;
+                [self updateOctaveLabel];
+            }
+            break;
+        default:
+        {
+            if (!theEvent.isARepeat) {
+                int noteNo = [self noteNo:key];
+                if (-1 != noteNo) {
+                    NoteEvent note;
+                    note.channel = _mixerChannel.number;
+                    note.noteNo = noteNo;
+                    note.velocity = 100;
+                    [_mixer sendNoteOn:&note];
+                }
+            }
+        }
+            break;
+    }
+}
+
+- (void)keyUp:(NSEvent *)theEvent
+{
+    unichar key = [theEvent.charactersIgnoringModifiers characterAtIndex:0];
+    int noteNo = [self noteNo:key];
+    if (-1 != noteNo) {
+        NoteEvent note;
+        note.channel = _mixerChannel.number;
+        note.noteNo = noteNo;
+        [_mixer sendNoteOff:&note];
+    }
 }
 
 #pragma mark NSTableViewDataSource
@@ -139,6 +263,29 @@
             case MixerChannelStatusKindSolo:
                 break;
         }
+    }
+}
+
+@end
+
+
+
+@implementation PresetTableView
+
+- (void)keyDown:(NSEvent *)theEvent
+{
+    // ESC
+    if (53 == theEvent.keyCode) {
+        [_controller cancelOperation:self];
+        return;
+    }
+    
+    unichar key = [theEvent.charactersIgnoringModifiers characterAtIndex:0];
+    if (NSUpArrowFunctionKey == key || NSDownArrowFunctionKey == key) {
+        [super keyDown:theEvent];
+    }
+    else {
+        [_controller keyDown:theEvent];
     }
 }
 
